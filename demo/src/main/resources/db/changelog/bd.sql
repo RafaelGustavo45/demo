af223ad98fc53e27425d35ef4485673263622088
Rafael Gustavo <88495051+RafaelGustavo45@users.noreply.github.com>
2024-11-19 13:00:02 -0300

    correcao da 01, arquivo de chancelog e banco de dados

--- demo/src/main/resources/db/changelog/bd.sql
+++ demo/src/main/resources/db/changelog/bd.sql
@@ -3,6 +3,6 @@ create database projeto;
 create table countrys
 (
     country_id int primary key AUTOINCREMENT;
-    contry_name varchar(255);
+    country_name varchar(255);
     region_id int;
 )
