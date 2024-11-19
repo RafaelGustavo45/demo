351ced8fc6ec06fe62a3ccdefe8f64a2212e8f96
Rafael Gustavo <88495051+RafaelGustavo45@users.noreply.github.com>
2024-11-19 13:40:00 -0300

    etapa 03 job_history

--- demo/src/main/resources/db/changelog/bd.sql
+++ demo/src/main/resources/db/changelog/bd.sql
@@ -2,14 +2,24 @@ create database projeto;
 
 create table countrys
 (
-    country_id int primary key AUTOINCREMENT;
-    country_name varchar(255);
+    country_id int primary key AUTOINCREMENT,
+    country_name varchar(255),
     region_id int
 )
 
 create table jobs (
-    job_id int primary key AUTOINCREMENT;
-    job_title varchar(255);
-    max_salary real;
+    job_id int primary key AUTOINCREMENT,
+    job_title varchar(255),
+    max_salary real,
     min_salary real
+)
+
+create table job_history(
+    employee_id primary key AUTOINCREMENT,
+    start_date DATE,
+    end_date DATE,
+    departament_id INT,
+    job_id INT,
+    FOREIGN KEY (job_id) REFERENCES jobs(job_id)
+    
 )
